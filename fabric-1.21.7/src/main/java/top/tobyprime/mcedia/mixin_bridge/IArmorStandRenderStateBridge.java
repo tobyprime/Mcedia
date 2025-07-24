3d2e5d12ae7a9c5fef504feeb3a3e40b12cf4257
toby <tobyprime0516@outlook.com>
2025-07-24 16:47:21 +0800

    refactor: 抽象 gradle

--- fabric-1.21.7/src/main/java/top/tobyprime/mcedia/mixin_bridge/IArmorStandRenderStateBridge.java
+++
@@ -1,9 +0,0 @@
-package top.tobyprime.mcedia.mixin_bridge;
-
-import org.jetbrains.annotations.Nullable;
-import top.tobyprime.mcedia.client.PlayerAgent;
-
-public interface IArmorStandRenderStateBridge {
-    public void mcdia$setAgent(@Nullable PlayerAgent playerAgent);
-    public @Nullable PlayerAgent mcdia$getAgent();
-}
