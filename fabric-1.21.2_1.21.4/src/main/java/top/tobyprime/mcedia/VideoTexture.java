33af076342d940ae3272a3e09648b99fec6efa0c
toby <tobyprime0516@outlook.com>
2025-07-26 07:43:27 +0800

    fix: 1.21.4 首屏不显示

--- fabric-1.21.2_1.21.4/src/main/java/top/tobyprime/mcedia/VideoTexture.java
+++ fabric-1.21.2_1.21.4/src/main/java/top/tobyprime/mcedia/VideoTexture.java
@@ -7,15 +7,13 @@ import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.system.MemoryUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.tobyprime.mcedia.core.VideoFrame;
 import top.tobyprime.mcedia.interfaces.ITexture;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
 
 import static com.mojang.blaze3d.platform.GlConst.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.opengl.GL11.*;
@@ -50,10 +48,23 @@ public class VideoTexture extends AbstractTexture implements ITexture {
         GlStateManager._texParameter(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         GlStateManager._texParameter(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         GlStateManager._texParameter(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
         GlStateManager._texImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, null);
-
         this.setFilter(true, false);
+        // 初始化为纯白色
+        ByteBuffer whiteBuffer = MemoryUtil.memAlloc(width * height * 4);
+        for (int i = 0; i < width * height; i++) {
+            whiteBuffer.put((byte) 0xFF); // R
+            whiteBuffer.put((byte) 0xFF); // G
+            whiteBuffer.put((byte) 0xFF); // B
+            whiteBuffer.put((byte) 0xFF); // A
+        }
+        whiteBuffer.rewind();
+        GlStateManager._pixelStore(GL_UNPACK_ALIGNMENT, 1);     // 每行像素对齐方式
+        GlStateManager._pixelStore(GL_UNPACK_ROW_LENGTH, 0);    // 行长度（0 = 紧密排列）
+        GlStateManager._pixelStore(GL_UNPACK_SKIP_ROWS, 0);     // 跳过的行数
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
+                GL_RGBA, GL_UNSIGNED_BYTE, whiteBuffer);
+        MemoryUtil.memFree(whiteBuffer);
     }
     public ResourceLocation getResourceLocation(){
         return this.resourceLocation;
