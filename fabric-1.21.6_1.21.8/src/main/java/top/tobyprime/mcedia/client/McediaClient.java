f2cc36b2c25f5d235b487551f55bfb08429c8a1f
Oyatsu <r03579@vip.qq.com>
2025-11-14 02:51:19 +0800

    优化

--- fabric-1.21.6_1.21.8/src/main/java/top/tobyprime/mcedia/client/McediaClient.java
+++ fabric-1.21.6_1.21.8/src/main/java/top/tobyprime/mcedia/client/McediaClient.java
@@ -1,8 +1,12 @@
 package top.tobyprime.mcedia.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.world.entity.EntityType;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import top.tobyprime.mcedia.Command.*;
+import top.tobyprime.mcedia.Mcedia;
+import top.tobyprime.mcedia.manager.BilibiliAuthManager;
 
 public class McediaClient implements ClientModInitializer {
 
