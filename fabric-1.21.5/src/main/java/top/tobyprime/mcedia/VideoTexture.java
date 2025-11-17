65486446db75050d7192d8d310999f89fe157848
toby <tobyprime0516@outlook.com>
2025-11-17 13:28:34 +0800

    feat: 优化上传性能

--- fabric-1.21.5/src/main/java/top/tobyprime/mcedia/VideoTexture.java
+++ fabric-1.21.5/src/main/java/top/tobyprime/mcedia/VideoTexture.java
@@ -1,55 +1,141 @@
 package top.tobyprime.mcedia;
 
-import com.mojang.blaze3d.platform.NativeImage;
+import com.mojang.blaze3d.opengl.GlStateManager;
+import com.mojang.blaze3d.opengl.GlTexture;
 import com.mojang.blaze3d.systems.GpuDevice;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.textures.GpuTexture;
 import com.mojang.blaze3d.textures.TextureFormat;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.tobyprime.mcedia.decoders.VideoFrame;
 import top.tobyprime.mcedia.interfaces.ITexture;
 
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL21.GL_PIXEL_UNPACK_BUFFER;
+
 public class VideoTexture extends AbstractTexture implements ITexture {
     private final Logger logger = LoggerFactory.getLogger(VideoTexture.class);
-    ResourceLocation resourceLocation;
+    private int width;
+    private int height;
+    // PBO 双缓冲
+    private final int[] pboIds = new int[2];
+    private ResourceLocation resourceLocation;
+    private int pboIndex = 0;
+    private boolean pboInitialized = false;
 
     public VideoTexture(ResourceLocation id) {
         super();
-        Minecraft.getInstance().getTextureManager().register(id, this);
         this.resourceLocation = id;
+        Minecraft.getInstance().getTextureManager().register(id, this);
         setSize(1920,1080);
     }
 
     public void setSize(int width,int height) {
-        if (texture==null || texture.getWidth(0) != width || texture.getHeight(0) != height) {
+        if (this.width != width || this.height != height) {
             resize(width, height);
         }
     }
 
     public void resize(int width, int height) {
-        logger.info("修改尺寸 {}x{}",width,height);
+        logger.info("修改尺寸 {}x{}", width, height);
+        this.width = width;
+        this.height = height;
+
         GpuDevice gpuDevice = RenderSystem.getDevice();
 
         this.texture = gpuDevice.createTexture(this.resourceLocation::toString, TextureFormat.RGBA8, width, height, 1);
         this.setClamp(true);
         this.setFilter(true, false);
+
+        releasePbo();
+        initPBO();
+        pboInitialized = true;
     }
 
-    public void upload(VideoFrame frame) {
-        if (this.texture == null) {
-            return;
+    private void initPBO() {
+        glGenBuffers(pboIds);
+        for (int i = 0; i < 2; i++) {
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pboIds[i]);
+            glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * 4, GL_STREAM_DRAW);
         }
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    }
+
+    public ResourceLocation getResourceLocation(){
+        return this.resourceLocation;
+    }
+
+
+    /**
+     * 异步上传 RGBA buffer 使用 PBO 双缓冲
+     */
+    public void uploadBuffer(ByteBuffer buffer) {
+        buffer.rewind();
+        if (!buffer.isDirect()) {
+            throw new IllegalArgumentException("ByteBuffer 必须是 direct 类型");
+        }
+
+        // 当前 PBO
+        int currPBO = pboIds[pboIndex];
+        int nextPBO = pboIds[(pboIndex + 1) % 2];
+
+        // 绑定当前 PBO
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, currPBO);
+
+        // 映射写入数据
+        ByteBuffer mapped = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+        mapped.put(buffer);
+        mapped.flip();
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+
+        // 绑定纹理 + 上一 PBO 异步上传到 GPU
+        GlStateManager._bindTexture(((GlTexture)this.texture).glId());
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, nextPBO);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height,
+                GL_RGBA, GL_UNSIGNED_BYTE, 0);
+
+        // 切换 PBO
+        pboIndex = (pboIndex + 1) % 2;
+
+        // 解绑
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    }
+
+    @Override
+    public void upload(@Nullable VideoFrame frame) {
+        if (frame == null) return;
+
         setSize(frame.width, frame.height);
-        GpuDevice gpuDevice = RenderSystem.getDevice();
-        frame.buffer.rewind();
-        gpuDevice.createCommandEncoder().writeToTexture(texture, frame.buffer.asIntBuffer(), NativeImage.Format.RGBA, 0, 0, 0, this.texture.getWidth(0), this.texture.getHeight(0));
+        GlStateManager._bindTexture(((GlTexture)this.texture).glId());
+        RenderSystem.assertOnRenderThread();
+
+        GlStateManager._pixelStore(GL_UNPACK_ALIGNMENT, 1);
+        GlStateManager._pixelStore(GL_UNPACK_ROW_LENGTH, 0);
+        GlStateManager._pixelStore(GL_UNPACK_SKIP_ROWS, 0);
+
+        uploadBuffer(frame.buffer);
         frame.close();
     }
 
-    public ResourceLocation getResourceLocation() {
-        return this.resourceLocation;
+    public void releasePbo() {
+        // 删除 PBO
+        if (pboInitialized) {
+            glDeleteBuffers(pboIds);
+            pboInitialized = false;
+        }
+    }
+
+    public void close() {
+        releasePbo();
+        super.close();
     }
 }
