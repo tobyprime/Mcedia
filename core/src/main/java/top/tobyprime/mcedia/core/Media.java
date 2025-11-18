2b6522975ebbcf0d1d1ea600a58a7b1120663a9f
toby <tobyprime0516@outlook.com>
2025-11-18 20:46:01 +0800

    fix: 再次修复不循环的问题

--- core/src/main/java/top/tobyprime/mcedia/core/Media.java
+++ core/src/main/java/top/tobyprime/mcedia/core/Media.java
@@ -90,7 +90,7 @@ public class Media implements Closeable {
                 if (!paused) {
                     IAudioData currFrame = decoder.getAudioQueue().poll();
                     if (currFrame == null) {
-                        if (looping && this.getDuration() >= this.getLength() && decoder.getDuration() != 0) {
+                        if (looping && this.decoder.isEnded() && decoder.getAudioQueue().isEmpty() && decoder.getDuration() != 0) {
                             // 如果播放结束，且需要循环则设置时间到 0
                             LOGGER.info("looping");
                             this.seek(0);
