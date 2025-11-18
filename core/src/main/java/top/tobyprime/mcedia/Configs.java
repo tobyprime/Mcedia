4cac9b5a22c6ffeaca76e7870ff72cd27046c77d
toby <tobyprime0516@outlook.com>
2025-11-18 11:51:57 +0800

    fix: 修复缓存过低而无法获取音频导致无法播放视频的问题

--- core/src/main/java/top/tobyprime/mcedia/Configs.java
+++ core/src/main/java/top/tobyprime/mcedia/Configs.java
@@ -21,12 +21,12 @@ public class Configs {
 
     // 缓冲与解码配置
     public static int DECODER_MAX_AUDIO_FRAMES = 1024;
-    public static int DECODER_MAX_VIDEO_FRAMES = 30;
+    public static int DECODER_MAX_VIDEO_FRAMES = 120;
 
 
     public static void fromProperties(Properties props) {
         Configs.MAX_PLAYER_COUNT = Integer.parseInt(props.getProperty("MAX_PLAYER_COUNT", String.valueOf(Configs.MAX_PLAYER_COUNT)));
-        Configs.ALLOW_DIRECT_LINK = Boolean.parseBoolean(props.getProperty("MAX_PLAYER_COUNT", String.valueOf(Configs.ALLOW_DIRECT_LINK)));
+        Configs.ALLOW_DIRECT_LINK = Boolean.parseBoolean(props.getProperty("ALLOW_DIRECT_LINK", String.valueOf(Configs.ALLOW_DIRECT_LINK)));
 
         Configs.DANMAKU_VISIBLE = Boolean.parseBoolean(props.getProperty("DANMAKU_VISIBLE", String.valueOf(Configs.DANMAKU_VISIBLE)));
         Configs.DANMAKU_DURATION = Float.parseFloat(props.getProperty("DANMAKU_DURATION", String.valueOf(Configs.DANMAKU_DURATION)));
